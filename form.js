ee63c7d1d0055b504039ba5ebbe77adc3c2c5b5d
Ruben Alba <rubenalbarevilla@gmail.com>
2022-08-05 09:38:38 +0200

    post hecho

---
+++ form.js
@@ -0,0 +1,23 @@
+document.getElementById("email-form").addEventListener("submit", addPost);
+
+function addPost(preventForm){
+    preventForm.preventDefault();
+    let name = document.getElementById("name").value;
+    let body = document.getElementById("field").value;
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      Accept: "text/plain, application/json, */*",
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ title: name, body: body }),
+  })
+    .then((response) => response.json())
+    .then((dataDeFormulario) => {
+      console.log(dataDeFormulario);
+      alert("Formulario enviado");
+    });
+  body.value = "";
+  name.value ="";
+}
