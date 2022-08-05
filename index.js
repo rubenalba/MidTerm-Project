409533df95935a136a2883cc2abb9119b2c29b39
rubenalba <rubenalbarevilla@gmail.com>
2022-08-05 11:37:42 +0200

    project desktop

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ getJson();
 
 function getJson() {
    
-    fetch("https://jsonplaceholder.typicode.com/posts/2")
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
     .then((response) => response.json())
     .then((res) => {
       
      let apiData = `
       <div class="section">
       <div class="container">
-        <h1>${res.id}</h1>
+        <h1> Id de la API: ${res.id}</h1>
         <div class="details">
-          <p>${res.body}</p>
+          <p>Contenido: ${res.body}</p>
         </div>
       </div>
     </div> 
